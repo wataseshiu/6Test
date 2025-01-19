12adb6a35817b5d1e7fd5423793227722af6a527
wataseshiu <kichikaraku@gmail.com>
2025-01-19 19:35:32 +0900

    Add files via upload

--- Scripts/CanvasController/TitleCanvasController.cs
+++ Scripts/CanvasController/TitleCanvasController.cs
@@ -1,5 +1,7 @@
 using R3;
 using R3.Triggers;
+using TMPro;
+using UI;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,11 +12,36 @@ namespace CanvasController
         public Subject<Unit> TitleClicked { get; } = new Subject<Unit>();
 
         [SerializeField] private Image titleImage;
+        [SerializeField] private TextMeshProUGUI versionText;
+        [SerializeField] private CustomButton volumeDialogButton;
+        [SerializeField] private DialogManager dialogManager;
+        
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
             //titleImageをクリックしたらtitleClickedを発行
-            titleImage.OnPointerClickAsObservable().Subscribe(_ => TitleClicked.OnNext(Unit.Default));
+            titleImage.OnPointerClickAsObservable().Subscribe(_ =>
+            {
+                Debug.Log("TitleImage Clicked");
+                TitleClicked.OnNext(Unit.Default);
+            });
+            
+            //volumeDialogButtonをクリックしたらダイアログ表示
+            volumeDialogButton.OnClickAsObservable().Subscribe(_ =>
+            {
+                dialogManager.CreateDialog( Dialog.ConfigVolume, 0);
+            });
+
+            versionText.text = $"Version: {Application.version}";
+        }
+
+        public void DeactivateTitleCanvas()
+        {
+            gameObject.SetActive(false);
+        }
+        public void ActivateTitleCanvas()
+        {
+            gameObject.SetActive(true);
         }
     }
 }
